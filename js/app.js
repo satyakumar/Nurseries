0ab19b4553421f251bdcbceb3eeac7c860c6d428
Satyakumar <satyakumar.kotipalli>
2016-02-11 09:28:53 +0530

    Revert the latest commit of remove # from url's because of no server for this project

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 var myNews = angular.module('myNews', ['ngRoute','ngCookies','ngResource']);
-myNews.config(function ($routeProvider,$locationProvider) {
-	$locationProvider.html5Mode(true);
+myNews.config(function ($routeProvider) {
     $routeProvider
             .when('/', {
                 templateUrl: 'templates/home.html',
